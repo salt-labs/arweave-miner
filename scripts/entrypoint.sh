6a8d4d5c77c8735d84e30f44c72a7664ec87b876
MAHDTech <mahdtech@saltlabs.tech>
2021-09-06 01:19:17 +0000

    Launch erlang VM manually

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,6 +5,10 @@
 # Description: Wrapper for running Arweave Miner
 ##################################################
 
+set -e
+set -u
+set -o pipefail
+
 # Get a script name for the logs
 export SCRIPT=${0##*/}
 
@@ -18,6 +22,10 @@ export ARWEAVE_PEERS="peer 188.166.200.45 peer 188.166.192.169 peer 163.47.11.64
 export ARWEAVE_CONFIG_DIR="${ARWEAVE_CONFIG_DIR:=$ARWEAVE_HOME/config}"
 export ARWEAVE_DATA_DIR="${ARWEAVE_DATA_DIR:=/data}"
 export ARWEAVE_SYNC_JOBS"${ARWEAVE_SYNC_JOBS:=3}"
+
+export RANDOMX_JIT=
+export ERL_EPMD_ADDRESS=127.0.0.1
+export NODE_NAME='arweave@127.0.0.1'
 
 # Arweave Utilities
 export ARWEAVE_TOOLS_REPO="https://github.com/francesco-adamo/arweave-tools"
@@ -63,15 +71,28 @@ git clone "${ARWEAVE_TOOLS_REPO}" "${ARWEAVE_HOME}/utilities/arweave-tools" || {
 	writeLog "WARNING" "Failed to clone latest Arweave Tools repository"
 }
 
-echo -e "Syncing weave and start mining"
+echo -e "Launching Erlang Virtual Machine..."
 
-"${ARWEAVE_HOME}/bin/start" \
-	data_dir "${ARWEAVE_DATA_DIR}" \
-	sync_jobs "${ARWEAVE_SYNC_JOBS}" \
-	mine \
-	mining_addr "${ARWEAVE_REWARDS_ADDRESS}" \
-	"${ARWEAVE_PEERS}" \
-	&
+"${ARWEAVE_HOME}/bin/arweave" \
+	daemon \
+	+Ktrue \
+	+A20 \
+	+SDio20 \
+	+sbwtvery_long \
+	+sbwtdcpuvery_long \
+	+sbwtdiovery_long \
+	+swtvery_low  \
+	+swtdcpuvery_low \
+	+swtdiovery_low \
+	+Bi \
+	-run \
+	ar \
+	main \
+		data_dir "${ARWEAVE_DATA_DIR}" \
+		sync_jobs "${ARWEAVE_SYNC_JOBS}" \
+		mine \
+		mining_addr "${ARWEAVE_REWARDS_ADDRESS}" \
+		"${ARWEAVE_PEERS}"
 
 #echo -e "Tailing logs"
 
@@ -79,20 +100,20 @@ echo -e "Syncing weave and start mining"
 
 while sleep 60; do
 
-	echo -e "Sleeping..."
-  
-  #ps aux | grep my_first_process | grep -q -v grep
-  #PROCESS_1_STATUS=$?
-  
-  #ps aux | grep my_second_process | grep -q -v grep
-  #PROCESS_2_STATUS=$?
-  
-  # If the greps above find anything, they exit with 0 status
-  # If they are not both 0, then something is wrong
-  #if [[ ${PROCESS_1_STATUS} -ne 0 ]] || [[ ${PROCESS_2_STATUS} -ne 0 ]]; 
-  #then
-  #  echo "One of the processes has failed!"
-  #  exit 1
-  #fi
+	echo -e "Checking Arweave status..."
+
+	#ps aux | grep my_first_process | grep -q -v grep
+	#PROCESS_1_STATUS=$?
+
+	#ps aux | grep my_second_process | grep -q -v grep
+	#PROCESS_2_STATUS=$?
+
+	# If the greps above find anything, they exit with 0 status
+	# If they are not both 0, then something is wrong
+	#if [[ ${PROCESS_1_STATUS} -ne 0 ]] || [[ ${PROCESS_2_STATUS} -ne 0 ]]; 
+	#then
+	#  echo "One of the processes has failed!"
+	#  exit 1
+	#fi
 
 done
