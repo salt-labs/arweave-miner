bbd96605cbbdd376ae93ed75ea100a341ef566cb
MAHDTech <mahdtech@saltlabs.tech>
2021-09-07 16:23:31 +0000

    Use correct variable name

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -17,7 +17,7 @@ export LOGLEVEL="${LOGLEVEL:=INFO}"
 
 # Arweave
 export ARWEAVE_HOME="${ARWEAVE_HOME:=/arweave}"
-export ARWEAVE_REWARDS_ADDRESS="${ARWEAVE_REWARDS_ADDRESS:=UNSET}"
+export ARWEAVE_REWARD_ADDRESS="${ARWEAVE_REWARD_ADDRESS:=UNSET}"
 export ARWEAVE_PEERS="peer 188.166.200.45 peer 188.166.192.169 peer 163.47.11.64 peer 139.59.51.59 peer 138.197.232.192"
 export ARWEAVE_CONFIG_DIR="${ARWEAVE_CONFIG_DIR:=$ARWEAVE_HOME/config}"
 export ARWEAVE_DATA_DIR="${ARWEAVE_DATA_DIR:=/data}"
@@ -86,9 +86,9 @@ esac
 set -m
 
 # Check the minimum required variables are populated
-checkVarEmpty "ARWEAVE_REWARDS_ADDRESS" "Arweave Rewards Address" && exit 1
+checkVarEmpty "ARWEAVE_REWARD_ADDRESS" "Arweave Rewards Address" && exit 1
 
-if [[ "${ARWEAVE_REWARDS_ADDRESS^^}" == "UNSET" ]];
+if [[ "${ARWEAVE_REWARD_ADDRESS^^}" == "UNSET" ]];
 then
 	echo "Arweave Rewards Address is not set!"
 	exit 1
@@ -102,7 +102,7 @@ git clone "${ARWEAVE_TOOLS_REPO}" "${ARWEAVE_HOME}/utilities/arweave-tools" || {
 
 echo -e "Arweave configuration parameters"
 
-echo -e "\tRewards Address: ${ARWEAVE_REWARDS_ADDRESS}"
+echo -e "\tRewards Address: ${ARWEAVE_REWARD_ADDRESS}"
 echo -e "\tHome: ${ARWEAVE_HOME}"
 echo -e "\tPeers: ${ARWEAVE_PEERS}"
 echo -e "\tConfig: ${ARWEAVE_CONFIG_DIR}"
@@ -134,7 +134,7 @@ echo -e "Launching Erlang Virtual Machine..."
 		data_dir "${ARWEAVE_DATA_DIR}" \
 		sync_jobs "${ARWEAVE_SYNC_JOBS}" \
 		mine \
-		mining_addr "${ARWEAVE_REWARDS_ADDRESS}" \
+		mining_addr "${ARWEAVE_REWARD_ADDRESS}" \
 		${ARWEAVE_PEERS}
 
 while true;
